ef22036421e562b94041817dd4af3f0665730d03
Aditya170305 <adityagupta.bpl08@gmail.com>
2026-08-18 19:40:52 +0530

    Largest Integer

---
+++ Arrays/L3471.java
@@ -0,0 +1,31 @@
+public class L3471 {
+    
+    public int largestInteger(int[] nums, int k) {
+
+        int freq [] = new int [51];
+
+        for(int i=0;i<=nums.length - k;i++){
+
+            boolean seen [] = new boolean [51];
+
+            for(int j=i;j<i+k;j++){
+
+                if(!seen[nums[j]]){
+                    freq[nums[j]]++;
+                    seen[nums[j]] = true;
+                }
+
+            }
+
+        }
+
+        for(int i=50;i>=0;i--){
+
+            if(freq[i] == 1) return i;
+
+        }
+
+        return -1;
+
+    }
+}
