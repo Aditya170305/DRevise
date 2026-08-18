4d25f1d86c5669e8d2e11809c9c6eab5a873dbe5
Aditya170305 <adityagupta.bpl08@gmail.com>
2026-08-18 20:51:08 +0530

    Fibonacci seris

---
+++ Dynamic Programming/FibonacciSeris.java
@@ -0,0 +1,34 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FibonacciSeris {
+
+    public static int fibonacci(int n , int dp [] ){
+
+        if(n <= 1){
+            return n;
+        }
+
+        if(dp[n] != -1) return dp[n];
+
+        return fibonacci(n - 1 , dp) + fibonacci(n - 2 , dp);
+
+    } 
+
+    public static void main(String[] args) {
+        
+        int n;
+
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+
+        int dp [] = new int [n + 1];
+
+        Arrays.fill(dp, -1);
+
+        System.out.println(fibonacci(n , dp));
+
+    }
+}
