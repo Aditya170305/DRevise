866846976c90954d304d2c45afe55c0db80d2ca9
Aditya170305 <adityagupta.bpl08@gmail.com>
2026-09-10 19:46:13 +0530

    Count Nodes equal to average of subtree

---
+++ Tree/L2265.java
@@ -0,0 +1,48 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class L2265 {
+    
+    int count = 0;
+
+    public int [] find(TreeNode root){
+
+        if(root == null){
+            return new int [] {0 , 0};
+        }
+
+        int [] left = find(root.left);
+        int [] right = find(root.right);
+
+        int sum = left[0] + right[0] + root.val;
+        int nodes = left[1] + right[1] + 1;
+
+        int avg = sum / nodes;
+
+        if(avg == root.val){
+            count++;
+        }
+
+        return new int [] {sum , nodes};
+
+    }
+
+    public int averageOfSubtree(TreeNode root) {
+        
+        find(root);
+
+        return count;
+
+    }
+
+}
