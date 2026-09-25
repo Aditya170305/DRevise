62bf5f3719042d68e85d19223b2d24fdaa0481e2
Aditya170305 <adityagupta.bpl08@gmail.com>
2026-09-25 11:32:52 +0530

    Find all duplicates in an array

---
+++ Arrays/FindAllDuplicatesInAnArray.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindAllDuplicatesInAnArray {
+    
+    public List<Integer> findDuplicates(int[] nums) {
+
+        List<Integer> ans = new ArrayList<Integer>();
+
+        for(int i=0;i<nums.length;i++){
+
+            int index = Math.abs(nums[i]) - 1;
+
+            if(nums[index] < 0){
+                ans.add(Math.abs(nums[i]));
+            }
+
+            else{
+                nums[index] = -nums[index];
+            }
+
+        }
+
+        return ans;
+
+    }
+    
+}
